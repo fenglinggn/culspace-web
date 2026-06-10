aefba9379e4783c53227f607acd8fd104031652a
xiefuling1226 <26987311@qq.com>
2026-06-10 21:28:40 +0800

    更新

--- assets/docx-preview-Cei2VYmU.js
+++ assets/docx-preview-Cei2VYmU.js
@@ -1,4 +1,4 @@
-import{g as fe}from"./index-HsvH3nYA.js";import{r as ge}from"./jszip.min-C5vuCE9N.js";var be=ge();const ke=fe(be);/*
+import{g as fe}from"./index-ypmKI6TA.js";import{r as ge}from"./jszip.min-DFlcRSNt.js";var be=ge();const ke=fe(be);/*
  * @license
  * docx-preview <https://github.com/VolodymyrBaydalka/docxjs>
  * Released under Apache License 2.0  <https://github.com/VolodymyrBaydalka/docxjs/blob/master/LICENSE>
--- assets/jszip.min-DFlcRSNt.js
+++ assets/jszip.min-DFlcRSNt.js
@@ -1,4 +1,4 @@
-import{c as bt}from"./index-HsvH3nYA.js";function vt(yt){throw new Error('Could not dynamically require "'+yt+'". Please configure the dynamicRequireTargets or/and ignoreDynamicRequires option of @rollup/plugin-commonjs appropriately for this require call to work.')}var kt={exports:{}};/*!
+import{c as bt}from"./index-ypmKI6TA.js";function vt(yt){throw new Error('Could not dynamically require "'+yt+'". Please configure the dynamicRequireTargets or/and ignoreDynamicRequires option of @rollup/plugin-commonjs appropriately for this require call to work.')}var kt={exports:{}};/*!
 
 JSZip v3.10.1 - A JavaScript class for generating and reading zip files
 <http://stuartk.com/jszip>
